84e6382daac4dab319c180366bef2d2e797dfc62
ImLecus <marcosgf2005@gmail.com>
2022-11-05 18:16:42 +0100

    Added modals when images get clicked

---
+++ script.js
@@ -0,0 +1,17 @@
+document.querySelectorAll(".row img").forEach(image => {
+    image.addEventListener("click", () => {
+        makeBigger(image);
+    })
+});
+document.querySelectorAll(".row2 img").forEach(image => {
+    image.addEventListener("click", () => {
+        makeBigger(image);
+    })
+});
+function makeBigger(i){
+    document.getElementById("topLayer").style.display = "block";
+    document.querySelector("#topLayer img").src = i.src;
+}
+document.querySelector("#topLayer").addEventListener("click",()=>{
+    document.getElementById("topLayer").style.display = "none";
+})
